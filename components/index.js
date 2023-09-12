e20270913aa285c4925085c62bee36d3c3bbf10d
Abhijeet <abhiabhii9001jk@gmail.com>
2023-09-12 18:43:57 +0530

    first commit

--- components/index.js
+++ components/index.js
@@ -1,19 +1,13 @@
 import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
 
 // home screen
-import Welcome from "./home/welcome/Welcome";
-import Nearbyjobs from "./home/nearby/Nearbyjobs";
-import Popularjobs from "./home/popular/Popularjobs";
 import Loginpage from "./home/login/Login";
 import Signinpage from "./home/signin/Signin";
 import Signuppage from "./home/signup/Signup";
 
 export {
   ScreenHeaderBtn,
-  Welcome,
-  Nearbyjobs,
   Signuppage,
-  Popularjobs,
   Loginpage,
   Signinpage,
 };
